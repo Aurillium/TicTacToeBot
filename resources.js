c6d6d8382bf2073e73fff52dbb838918b482409c
Memphis Marshall <memphism2003@gmail.com>
2022-07-12 22:20:56 +0800

    Added random choice function

--- resources.js
+++ resources.js
@@ -6,3 +6,7 @@ exports.message_embed = function(description, colour="#FF0000") {
 		.setDescription(description);
 }
 exports.sleep = ms => new Promise(r => setTimeout(r, ms));
+
+exports.random_choice = function(array) {
+	return array[Math.floor((Math.random()*array.length))];
+}
