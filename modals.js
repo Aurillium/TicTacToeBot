504a43ed48801d345b34c7ba9a909b65925ef663
Memphis Marshall <memphism2003@gmail.com>
2022-07-12 22:43:00 +0800

    Fixed modals again

--- modals.js
+++ modals.js
@@ -3,5 +3,6 @@ const fs = require('node:fs');
 const modal_files = fs.readdirSync('./modals').filter(file => file.endsWith('.js'));
 
 for (const file of modal_files) {
-	exports[file.slice(0, -3)] = require("./modals/" + file).modal;
+    var mod = require("./modals/" + file);
+	exports[mod.modal.customId] = mod.modal;
 }
